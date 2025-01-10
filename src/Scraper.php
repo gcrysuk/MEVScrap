94e366ae2e75f451e46209134de45ae1bfd08da8
gcrysuk <gabrielcrysuk@gmail.com>
2025-01-10 17:02:42 -0300

    version1

--- src/Scraper.php
+++ src/Scraper.php
@@ -10,26 +10,35 @@ class Scraper
         $this->cookieFile = __DIR__ . '/../storage/cookies.txt';
     }
 
-    public function login($username, $password)
-    {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $this->config['login_url']);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query([
-            'usuario' => $username,
-            'clave' => $password
-        ]));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_COOKIEJAR, $this->cookieFile);
-        curl_setopt($ch, CURLOPT_COOKIEFILE, $this->cookieFile);
-        curl_setopt($ch, CURLOPT_USERAGENT, $this->config['user_agent']);
+   public function login($username, $password)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $this->config['login_url']);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query([
+        'usuario' => $username,
+        'clave' => $password
+    ]));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_COOKIEJAR, $this->cookieFile);
+    curl_setopt($ch, CURLOPT_COOKIEFILE, $this->cookieFile);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_USERAGENT, $this->config['user_agent']);
 
+    $response = curl_exec($ch);
+
+    // Si hay redirección explícita a POSLoguin.asp
+    if (strpos($response, 'POSLoguin.asp') !== false) {
+        curl_setopt($ch, CURLOPT_URL, 'https://mev.scba.gov.ar/POSLoguin.asp');
         $response = curl_exec($ch);
-        curl_close($ch);
-
-        return $response;
     }
+
+    curl_close($ch);
+
+    file_put_contents(__DIR__ . '/../storage/login_response.html', $response);
+
+    return $response;
+}
 
     public function fetchData($filters)
     {
