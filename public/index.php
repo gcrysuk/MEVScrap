567115a0c78bce2b8d71ce76436f3a149e656212
gcrysuk <gabrielcrysuk@gmail.com>
2025-01-10 16:14:22 -0300

    version1

---
+++ public/index.php
@@ -0,0 +1,18 @@
+#### 4. `public/index.php`
+```php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Principal</title>
+</head>
+<body>
+    <h1>Bienvenido al Sistema de Scraping</h1>
+    <ul>
+        <li><a href="login.php">Iniciar Sesión</a></li>
+        <li><a href="search.php">Realizar Búsquedas</a></li>
+    </ul>
+</body>
+</html>
+```
